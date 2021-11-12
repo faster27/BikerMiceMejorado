63788dd03e413e7cc2cd7f5a4a9f1fb65c27461c
Katerin <katerin0120paz@gmail.com>
2021-11-12 18:42:57 -0500

    personalizando marcas y modificanzo zoom del mapa

--- app/src/main/java/com/example/BikerMice/MapsActivity.java
+++ app/src/main/java/com/example/BikerMice/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.BikerMice.databinding.ActivityMapsBinding;
@@ -30,22 +31,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng tulua = new LatLng(4.0856667, -76.1972779);
+        mMap.addMarker(new MarkerOptions().position(tulua).title("Municipio de Tuluá").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+        LatLng Buga = new LatLng(3.9000578, -76.302013);
+        mMap.addMarker(new MarkerOptions().position(Buga).title("Buga").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+        LatLng Andalucia = new LatLng(4.1694199, -76.1670623);
+        mMap.addMarker(new MarkerOptions().position(Andalucia).title("Andalucia - Valle").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+
+        LatLng SanPedro = new LatLng(3.9955682, -76.2280495);
+        mMap.addMarker(new MarkerOptions().position(SanPedro).title("San pedro - Valle").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tulua,10));
     }
 }
